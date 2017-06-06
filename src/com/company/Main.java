a3136439370f9a6fae56224d6b6d96f10b9eb30f
chao4kakaluote <zhong15514766480@163.com>
2017-06-06 17:27:10 +0800

    add 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,5 +7,6 @@ public class Main
              System.out.print("Hello World2!");
              System.out.println("Hello World3!");
              System.out.println("Hello World4!");
+            System.out.println("Hello World4!");
     }
 }
